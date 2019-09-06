fa08b899e6231ccc4b3e733599b0d105758fe1e4
Monster <896895671@qq.com>
2019-09-06 10:54:37 +0800

    Add command of SoftWareUpgrade

---
+++ x/upgrade/client/utils.go
@@ -0,0 +1,34 @@
+package client
+
+import (
+	"fmt"
+
+	sdk "github.com/orientwalt/htdf/types"
+	"github.com/orientwalt/htdf/x/upgrade"
+)
+
+type UpgradeInfoOutput struct {
+	CurrentVersion    upgrade.VersionInfo `json:"current_version"`
+	LastFailedVersion uint64              `json:"last_failed_version"`
+	UpgradeInProgress sdk.UpgradeConfig   `json:"upgrade_in_progress"`
+}
+
+func NewUpgradeInfoOutput(currentVersion upgrade.VersionInfo, lastFailedVersion uint64, upgradeInProgress sdk.UpgradeConfig) UpgradeInfoOutput {
+	return UpgradeInfoOutput{
+		currentVersion,
+		lastFailedVersion,
+		upgradeInProgress,
+	}
+}
+
+func (p UpgradeInfoOutput) String() string {
+	success := "fail"
+	if p.CurrentVersion.Success {
+		success = "success"
+	}
+	return fmt.Sprintf(`Upgrade Info:
+  Current Version[%v]:  %s     
+  Last Failed Version:  %v
+  Upgrade In Progress:  %s`,
+		success, p.CurrentVersion.UpgradeInfo, p.LastFailedVersion, p.UpgradeInProgress)
+}
