c5a882cafba9ee10436e6957763c8db397ab2c50
Cocer <896895671@qq.com>
2019-12-02 14:58:27 +0800

    Repair unit test of crypto

--- crypto/keys/types_test.go
+++ crypto/keys/types_test.go
@@ -26,7 +26,7 @@ func Test_writeReadLedgerInfo(t *testing.T) {
 	assert.NoError(t, err)
 	assert.Equal(t, "44'/118'/5'/0/1", path.String())
 	assert.Equal(t,
-		"cosmospub1addwnpepqddddqg2glc8x4fl7vxjlnr7p5a3czm5kcdp4239sg6yqdc4rc2r5wmxv8p",
+		"htdfpub1addwnpepqddddqg2glc8x4fl7vxjlnr7p5a3czm5kcdp4239sg6yqdc4rc2r5uw66wh",
 		types.MustBech32ifyAccPub(lInfo.GetPubKey()))
 
 	// Serialize and restore
