9ee02da760d200934d70178439b28858c6122edb
gustavkkk <gustav0125@outlook.com>
2020-03-11 11:39:46 +0800

    recover original

--- x/mint/wire.go
+++ x/mint/wire.go
@@ -9,9 +9,11 @@ import (
 func RegisterCodec(cdc *codec.Codec) {
 	// Not Register mint codec in app, deprecated now
 	//cdc.RegisterConcrete(Minter{}, "htdf/mint/Minter", nil)
-	cdc.RegisterConcrete(&Params{}, "mint/Params", nil)
+	cdc.RegisterConcrete(&Params{}, "htdf/mint/Params", nil)
+	cdc.RegisterConcrete(&sdk.Dec{}, "htdf/mint/rewards", nil)
+	// cdc.RegisterConcrete(&Params{}, "mint/Params", nil)
 	cdc.RegisterConcrete(&BlockReward{}, "mint/BlockReward", nil)
-	cdc.RegisterConcrete(&sdk.Dec{}, "types/Dec", nil)
+	// cdc.RegisterConcrete(&sdk.Dec{}, "types/Dec", nil)
 	cdc.RegisterConcrete(&sdk.Int{}, "types/Int", nil)
 }
 
